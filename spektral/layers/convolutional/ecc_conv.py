fix masking

spektral/layers/convolutional/ecc_conv.py
@@ -181,8 +181,11 @@ class ECCConv(Conv):
             output = K.bias_add(output, self.bias)
 
         # this doesn't work as expected (passing `None` to `mask` doesn't make it None. It makes it [None]*3, which makes it think that there's a mask.
-        if mask is not None or len(tf.where(mask != None)) > 0:
-            output *= tf.cast(mask[0], tf.float32)
+        if mask is not None and len(tf.where(mask != None)) > 0:
+            try:
+                output *= tf.cast(mask[0], tf.float32)
+            except ValueError:
+                output = output # pass
         output = self.activation(output)
 
         return output