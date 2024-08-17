39ded449b7431c40738a7a60521c5096c4c483b1
MirzaBicer <mirzabicer.dev@gmail.com>
2024-08-17 22:12:12 +0300

    Update arma_conv.py

--- spektral/layers/convolutional/arma_conv.py
+++ spektral/layers/convolutional/arma_conv.py
@@ -140,8 +140,13 @@ class ARMAConv(Conv):
         output = K.stack(output, axis=-1)
         output = K.mean(output, axis=-1)
 
-        if mask is not None:
-            output *= mask[0]
+        # this doesn't work as expected (passing `None` to `mask` doesn't make it None. It makes it [None]*3, which makes it think that there's a mask.
+        mask_none = tf.where(mask != None and mask != 'None')
+        if mask is not None and mask_none.shape[0] is not None and mask_none.shape[0] > 0:
+            try:
+                output *= tf.cast(mask[0], tf.float32)
+            except ValueError:
+                output = output # pass
         output = self.activation(output)
 
         return output
