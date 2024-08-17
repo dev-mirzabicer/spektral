fix masking

spektral/layers/convolutional/cheb_conv.py
@@ -128,7 +128,7 @@ class ChebConv(Conv):
         if self.use_bias:
             output = KB.bias_add(output, self.bias)
         # this doesn't work as expected (passing `None` to `mask` doesn't make it None. It makes it [None]*3, which makes it think that there's a mask.
-        if mask is not None and len(tf.where(mask != None)) > 0:
+        if mask is not None and tf.where(mask != None).shape[0] > 0:
             try:
                 output *= tf.cast(mask[0], tf.float32)
             except ValueError: