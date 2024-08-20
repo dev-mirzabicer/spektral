6f9a1c5a226269ba87987432ae48f0d466d9c8ed
MirzaBicer <mirzabicer.dev@gmail.com>
2024-08-20 17:37:25 +0300

    Update convolution.py

--- spektral/utils/convolution.py
+++ spektral/utils/convolution.py
@@ -125,7 +125,7 @@ def gcn_filter_tf(A, symmetric=True):
     # Add self-loops
     n = tf.shape(A)[0]
     edge_index = tf.concat([A.indices, tf.stack([tf.range(n, dtype=A.indices.dtype), tf.range(n, dtype=A.indices.dtype)], axis=1)], axis=0)
-    edge_weight = tf.concat([A.values, tf.ones(n, dtype=A.indices.dtype)], axis=0)
+    edge_weight = tf.concat([A.values, tf.ones(n, dtype=A.values.dtype)], axis=0)
     
     A_hat = tf.sparse.SparseTensor(
         indices=edge_index,
